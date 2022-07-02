6345594160150cfaaf2926b713546b674b93d118
prajakta1205 <prajakta12wankhede@gmail.com>
2022-07-02 21:38:29 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
 import React from 'react';
-import './components/category-item.style.scss';
+import CategoryItem from './components/category-item/category-item.component';
+import './categories-item.style.scss'
 
 const App = () => {
   const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-    },
-    {
-      id: 4,
-      title: 'Womens',
-    },
-    {
-      id: 5,
-      title: 'Mens',
-    },
+   
+  {
+    "id": 1,
+    "title": "hats",
+    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+  },
+  {
+    "id": 2,
+    "title": "jackets",
+    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+  },
+  {
+    "id": 3,
+    "title": "sneakers",
+    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+  },
+  {
+    "id": 4,
+    "title": "womens wear",
+    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+  },
+  {
+    "id": 5,
+    "title": "mens wear",
+    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+  }
   ];
 
   return (
-    <div className="categories-container">
-      {
-        categories.map(({title}) =>(
-          <div className="category-container">
-          {/*<imag/>*/}
-          <div className="background-image"/>
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-        ))
-      }
+    <div className='categories-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
     </div>
   );
 };
