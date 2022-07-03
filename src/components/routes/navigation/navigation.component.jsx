99868f0f1fa72e9940b76cef5ef27c1a0272a65d
prajakta1205 <prajakta12wankhede@gmail.com>
2022-07-04 00:41:53 +0530

    React Outlet

---
+++ src/components/routes/navigation/navigation.component.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom"
+
+const Navigation=()=>{
+    return(
+      <div>
+        <h1>Navigation Bar</h1>
+        <Outlet/>
+      </div>
+    )
+  }
+
+ export default Navigation 
