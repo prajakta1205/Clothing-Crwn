10ee097507536a21fccad06ea6523f96e6f11588
prajakta1205 <prajakta12wankhede@gmail.com>
2022-07-08 02:15:53 +0530

    minor changes

--- src/components/routes/authentication/authentication.component.jsx
+++ src/components/routes/authentication/authentication.component.jsx
@@ -1,13 +1,15 @@
 
-  import SignUpForm from '../../sign-in-form/sign-in-form.component';
+  import SignUpForm from '../../sign-up-form/sign-up-form.component';
   import SignInForm from '../../sign-in-form/sign-in-form.component';
-  
+  import './authentication.style.scss'
+
+
   const Authentication = () => {
  
   
     return (
-      <div>
-        <h1>Sign In Page</h1>
+      <div className='authentication-container'>
+       
         <SignInForm />
         <SignUpForm />
       </div>
